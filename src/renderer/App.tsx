a4c8f376715b117eb41e62ed6bb5bafd58aa938c
nestor <nestor.calvo@udea.edu.co>
2023-09-14 13:43:34 +0200

    Add Home route

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,6 +8,7 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
