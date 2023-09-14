018b35a55fa9ff51ccf2a2d605f7ea9628532742
nestor <nestor.calvo@udea.edu.co>
2023-09-14 12:32:14 +0200

    Login page connected to app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,13 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
