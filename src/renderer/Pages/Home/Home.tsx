b06101ade28977de2c6cda1aeef8dd80a692bde5
nestor <nestor.calvo@udea.edu.co>
2023-09-14 10:19:18 +0200

    Page separation of Home

---
+++ src/renderer/Pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import icon from '../../../../assets/icon.png';
+
+function Home() {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>Eval Virtual Udea</h1>
+    </div>
+  );
+}
+export default Home;
