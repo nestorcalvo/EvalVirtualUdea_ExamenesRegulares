cf989c534918f6cf5a9df9bbb41f34adbdae3612
nestorcalvo <nestor.calvo@udea.edu.co>
2023-12-05 10:24:32 -0500

    jQuery installed and added to the render

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+(window as any).$ = require('jquery');
+(window as any).jQuery = require('jquery');
+
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(<App />);
