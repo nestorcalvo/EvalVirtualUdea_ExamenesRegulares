9c5f3f40ed60d3443bc42ca58fad83be74b4818f
nestor <nestor.calvo@udea.edu.co>
2023-12-28 20:38:17 -0500

    Countdown when warning created

--- src/renderer/Pages/WarnPage/WarnPage.tsx
+++ src/renderer/Pages/WarnPage/WarnPage.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function WarnPage() {
   const [warning, setWarning] = useState('');
   const [warningType, setWarningType] = useState('');
+  const [timer, setTimer] = useState(10);
   window.electron.ipcRenderer.once('open_window', (args) => {
     if (Array.isArray(args)) {
       setWarningType('software');
@@ -10,11 +11,27 @@ function WarnPage() {
       setWarning(warn);
     }
   });
+  const intervalRef: any = useRef(); // Add a ref to store the interval id
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setTimer((t) => t - 1);
+    }, 1000);
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  // Add a listener to `timeLeft`
+  useEffect(() => {
+    if (timer <= 0) {
+      clearInterval(intervalRef.current);
+    }
+  }, [timer]);
 
   return (
     <div>
       <h1>ALERTA</h1>
       {warningType === 'software' && <h1>{warning}</h1>}
+      <h1>{timer}</h1>
     </div>
   );
 }
