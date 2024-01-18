c2a53ed16ed873f9daa8991831beedfa6cf0409d
nestorcalvo <nestor.calvo@udea.edu.co>
2024-01-18 12:00:17 -0500

    Fix height and width of the camera

--- src/renderer/Components/Camera/Camera.tsx
+++ src/renderer/Components/Camera/Camera.tsx
@@ -4,12 +4,13 @@ import Webcam from 'react-webcam';
 const videoConstraints = {
   width: 480,
   height: 240,
-
   facingMode: 'user',
 };
 function Camera() {
   return (
-    <div>
+    <div
+      style={{ height: videoConstraints.height, width: videoConstraints.width }}
+    >
       <Webcam audio={false} videoConstraints={videoConstraints} />
     </div>
   );
