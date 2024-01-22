8b172d8ac33b2108644370a92031bdf9de437416
nestorcalvo <nestor.calvo@udea.edu.co>
2024-01-22 18:32:24 -0500

    Change in style of Home page

--- src/renderer/Pages/Home/Home.tsx
+++ src/renderer/Pages/Home/Home.tsx
@@ -1,60 +1,87 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 // import './Home.css';
-import { useLocation } from 'react-router-dom';
 import Camera from 'renderer/Components/Camera/Camera';
 import Information from 'renderer/Components/Information/Information';
-import { Divider, Paper, Button } from '@mui/material';
+import { Container, Button, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-import Stack from '@mui/material/Stack';
 import { URL_EXAM } from 'utils/constants';
 
-import icon from '../../../../assets/icon.png';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
-
 function Home() {
-  // const { state } = useLocation();
-  // const token = state?.token;
   const renderExamPage = () => {
     window.electron.ipcRenderer.sendMessage('start_exam', URL_EXAM);
   };
   return (
-    <Box sx={{ width: '100%' }} justifyContent="center">
-      <div>
-        <img width="50" height="50" alt="icon" src={icon} />
-      </div>
-      <Stack
-        spacing={{ xs: 1, sm: 2 }}
-        divider={<Divider orientation="vertical" flexItem />}
-        direction="row"
-        useFlexGap
-        flexWrap="wrap"
-        alignItems="center"
-      >
-        <Item>
+    <Container maxWidth="sm" style={{ justifyContent: 'center' }}>
+      <Box display="flex" flexWrap="wrap" justifyContent="center">
+        <Typography
+          variant="body1"
+          style={{
+            color: '#0B8983',
+            display: 'flex',
+            textAlign: 'center',
+          }}
+          fontSize="h4.fontSize"
+        >
+          Aplicación para examenes virtuales
+        </Typography>
+        <Typography
+          variant="body1"
+          style={{ color: '#49D5BB', display: 'flex', textAlign: 'center' }}
+          fontSize="h4.fontSize"
+        >
+          Universidad de Antioquia
+        </Typography>
+      </Box>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <Box
+          component="div"
+          sx={{
+            whiteSpace: 'wrap',
+            overflowX: 'auto',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            my: 2,
+            p: 1,
+            bgcolor: (theme) =>
+              theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
+            color: (theme) =>
+              theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
+            border: '1px solid',
+            borderColor: (theme) =>
+              theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
+            borderRadius: 2,
+            fontSize: '0.875rem',
+            fontWeight: '700',
+          }}
+        >
           <Information />
-        </Item>
-        <Item>
+        </Box>
+        <Box component="div" display="flex" justifyContent="center">
           <Camera />
-        </Item>
-      </Stack>
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        onClick={renderExamPage}
-        sx={{ mx: 'auto', maxWidth: '20%', bgcolor: 'success.main' }}
-      >
-        Iniciar Examen
-      </Button>
-    </Box>
+        </Box>
+        <Box
+          component="div"
+          display="flex"
+          justifyContent="center"
+          sx={{ p: 1 }}
+        >
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={renderExamPage}
+            sx={{
+              mx: 'auto',
+              backgroundColor: '#0B8983',
+              maxWidth: '40%',
+            }}
+          >
+            Iniciar Examen
+          </Button>
+        </Box>
+      </div>
+    </Container>
   );
 }
 export default Home;
