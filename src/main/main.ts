51b1fe5284e366c041a4e8b430400790e83fae08
nestor <nestor.calvo@udea.edu.co>
2024-02-02 23:30:30 -0500

    Check the updates

--- src/main/main.ts
+++ src/main/main.ts
@@ -553,6 +553,7 @@ app
     //   console.log(info);
     // });
     // autoUpdater.checkForUpdates();
+    autoUpdater.checkForUpdates();
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
