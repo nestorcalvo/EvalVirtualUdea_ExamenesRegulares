97d85540ba190ff8eecc301787fbca19bf17d899
nestorcalvo <nestor.calvo@udea.edu.co>
2024-02-05 14:02:01 -0500

    Some comments removed

--- src/main/main.ts
+++ src/main/main.ts
@@ -253,18 +253,7 @@ const checkSoftware = async () => {
   }
   return procesoFound;
 };
-// const installExtensions = async () => {
-//   const installer = require('electron-devtools-installer');
-//   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-//   const extensions = ['REACT_DEVELOPER_TOOLS'];
 
-//   return installer
-//     .default(
-//       extensions.map((name) => installer[name]),
-//       forceDownload
-//     )
-//     .catch(console.log);
-// };
 const createWarnWindow = async (
   parent: BrowserWindow,
   show: boolean,
